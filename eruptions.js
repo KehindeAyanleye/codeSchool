d33a45bc11fb01cd7e21221ed832c7c67513b877
Kehinde Ayanleye <Kehindeayanleye@gmail.com>
2014-10-01 17:40:49 +0100

    Create eruptions.js

---
+++ eruptions.js
@@ -0,0 +1,6 @@
+var eruption1 = 112;
+var eruption2 = 148;
+var eruption3 = 160;
+var numEruptions = 3;
+var avg = (eruption1 + eruption2 + eruption3)/numEruptions;
+console.log("\"Old Faithful\" at Yellowstone National Park\nEruption 1\: " + eruption1 + "\nEruption 2\: " + eruption2 + "\nEruption 3\: " + eruption3 + "\nAverage eruption height today\: " + avg);
